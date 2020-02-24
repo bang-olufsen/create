30e175d744e19592f99ec334a357714311eb642b
Tuomas Hämäläinen <tuomashamalainen92@gmail.com>
2020-02-24 16:17:28 +0200

    Make static hostnames lower case

--- beocreate_essentials/pi_system_tools.js
+++ beocreate_essentials/pi_system_tools.js
@@ -66,6 +66,7 @@ function setHostname(productName, callback) {
 				n = n.replace(/_/g, "-"); // Replace underscores with hyphens
 				n = n.replace(/[^\x00-\x7F]/g, ""); // Remove non-ascii characters
 				n = n.replace(/-+$/g, ""); // Remove hyphens from the end of the name.
+				n = n.toLowerCase(); // Make lower case.
 				
 				exec("/usr/bin/hostnamectl set-hostname --static "+n, function(error, stdout, stderr) {
 					

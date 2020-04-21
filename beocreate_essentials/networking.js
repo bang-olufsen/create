6be86ca4013d7e3465f9c3296a44ae7c758ecaaf
Tuomas Hämäläinen <tuomashamalainen92@gmail.com>
2020-04-22 00:12:26 +0300

    Support getting and setting Wi-Fi enabled status

--- beocreate_essentials/networking.js
+++ beocreate_essentials/networking.js
@@ -31,6 +31,7 @@ var ifconfig = require('wireless-tools/ifconfig');
 
 var networking = module.exports = {
 	getWifiStatus: getWifiStatus,
+	setWifiStatus: setWifiStatus,
 	getEthernetStatus: getEthernetStatus,
 	getCountry: getCountry,
 	setCountry: setCountry,
@@ -62,48 +63,68 @@ if (fs.existsSync("/etc/wpa_supplicant/wpa_supplicant.conf")) {
 
 
 // GET NETWORK STATUS
-// Does the system have Wi-Fi, is it connected and if so, where? Same for ethernet.
+// Does the system have Wi-Fi, is it on, is it connected and if so, where? Same for ethernet.
 function getWifiStatus(callback) {
 	if (callback) {
-		iwconfig.status('wlan0', function(err, status) {
+		ifconfig.status('wlan0', function(err, status) { // Use ifconfig to check if Wi-Fi is on or not.
 			if (err) {
 				// The system has no Wi-Fi capabilities.
 				callback(null, err);
 			} else {
-				wifiStatus = {up: true, connected: false};
-				if (status.ssid && !status.unassociated) {
-					wifiStatus.ssid = decodeURIComponent(escape(status.ssid.decodeEscapeSequence()));
-					if (status.mode != "master") wifiStatus.connected = true;
-					wifiStatus.quality = status.quality;
-				}
-				wlan0 = os.networkInterfaces().wlan0;
-				if (wlan0) {
-					for (var i = 0; i < wlan0.length; i++) {
-						if (wlan0[i].family == "IPv4") {
-							wifiStatus.mac = wlan0[i].mac;
-							wifiStatus.ipv4 = {address: wlan0[i].address, subnetmask: wlan0[i].netmask};
+				wifiStatus = {up: false, connected: false};
+				if (status.up) wifiStatus.up = true;
+				iwconfig.status('wlan0', function(err, status) {
+					if (err) {
+						// The system has no Wi-Fi capabilities.
+						callback(null, err);
+					} else {
+						if (status.ssid && !status.unassociated) {
+							wifiStatus.ssid = decodeURIComponent(escape(status.ssid.decodeEscapeSequence()));
+							if (status.mode != "master") wifiStatus.connected = true;
+							wifiStatus.quality = status.quality;
 						}
-						if (wlan0[i].family == "IPv6") {
-							wifiStatus.ipv6 = {address: wlan0[i].address, subnetmask: wlan0[i].netmask};
+						wlan0 = os.networkInterfaces().wlan0;
+						if (wlan0) {
+							for (var i = 0; i < wlan0.length; i++) {
+								if (wlan0[i].family == "IPv4") {
+									wifiStatus.mac = wlan0[i].mac;
+									wifiStatus.ipv4 = {address: wlan0[i].address, subnetmask: wlan0[i].netmask};
+								}
+								if (wlan0[i].family == "IPv6") {
+									wifiStatus.ipv6 = {address: wlan0[i].address, subnetmask: wlan0[i].netmask};
+								}
+							}
+							dhcp = readDHCPSettings("wifi");
+							if (dhcp && dhcp.Network) {
+								if (dhcp.Network.DHCP) {
+									// Automatic settings.
+									wifiStatus.ipSetup = {auto: true};
+								} else {
+									// Manual settings.
+									wifiStatus.ipSetup = {auto: false};
+									wifiStatus.ipSetup.address = dhcp.Network.Address.split("/")[0];
+									wifiStatus.ipSetup.subnetmask = CIDRToSubnetmask(dhcp.Network.Address.split("/")[1]);
+									wifiStatus.ipSetup.dns = dhcp.Network.DNS;
+									wifiStatus.ipSetup.router = dhcp.Network.Gateway;
+								}
+							}
 						}
+						callback(wifiStatus, false);
 					}
-					dhcp = readDHCPSettings("wifi");
-					if (dhcp && dhcp.Network) {
-						if (dhcp.Network.DHCP) {
-							// Automatic settings.
-							wifiStatus.ipSetup = {auto: true};
-						} else {
-							// Manual settings.
-							wifiStatus.ipSetup = {auto: false};
-							wifiStatus.ipSetup.address = dhcp.Network.Address.split("/")[0];
-							wifiStatus.ipSetup.subnetmask = CIDRToSubnetmask(dhcp.Network.Address.split("/")[1]);
-							wifiStatus.ipSetup.dns = dhcp.Network.DNS;
-							wifiStatus.ipSetup.router = dhcp.Network.Gateway;
-						}
-					}
-				}
-				callback(wifiStatus, false);
+				});
 			}
+		});
+	}
+}
+
+function setWifiStatus(power, callback) {
+	if (power == true) {
+		exec('ifconfig wlan0 up', function(error, stdout, stderr){
+		    if (callback) callback(true, error);
+		});
+	} else {
+		exec('ifconfig wlan0 down', function(error, stdout, stderr){
+		    if (callback) callback(false, error);
 		});
 	}
 }

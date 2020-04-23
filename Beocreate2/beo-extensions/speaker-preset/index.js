8579f01d29f91be3711a0dceeb4134394067949c
Tuomas Hämäläinen <tuomashamalainen92@gmail.com>
2020-04-24 01:30:05 +0300

    Collect selected speaker preset for statistics

--- Beocreate2/beo-extensions/speaker-preset/index.js
+++ Beocreate2/beo-extensions/speaker-preset/index.js
@@ -184,6 +184,10 @@ var beoDSP = require('../../beocreate_essentials/dsp');
 				} else {
 					beo.bus.emit("ui", {target: "speaker-preset", header: "presetApplied", content: {presetID: event.content.presetID}});
 				}
+				if (extensions["hifiberry-debug"] &&
+					extensions["hifiberry-debug"].reportActivation) {
+					extensions["hifiberry-debug"].reportActivation("beo-speaker-preset_"+event.content.presetID, true);
+				}
 				beo.bus.emit("daisy-chain", {header: "disableDaisyChaining", content: {reason: "speakerPresetSelected"}});
 			}
 			

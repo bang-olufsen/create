408a4d2d23cca845fb7d61f8578de9792473499b
Tuomas Hämäläinen <tuomashamalainen92@gmail.com>
2020-07-22 17:00:53 +0300

    Update system info to use standard menu items

--- Beocreate2/beo-extensions/hifiberry-debug/hifiberry-debug-client.js
+++ Beocreate2/beo-extensions/hifiberry-debug/hifiberry-debug-client.js
@@ -61,30 +61,23 @@ function collect() {
 }
 
 
-function create_line(block, d1,d2) {
-	
-	var div1 = document.createElement("div");
-	div1.innerHTML = d1;
-	div1.setAttribute('class', 'hifiberry-debug-sysinfo-left')
-
-	var div2 = document.createElement("div");
-	div2.innerHTML = d2;
-	div2.setAttribute('class', 'hifiberry-debug-sysinfo-right')
-	
-	block.appendChild(div1);
-	block.appendChild(div2);
-}
-
-
 function updateSystemInfo(info) {
 		block = document.getElementById("hifiberry-debug-sysinfo");
 		block.innerHTML = "";
-		
 		for (var i = 0; i <info.length; i++) {
-			var row = document.createElement("div");
-			row.setAttribute('class', 'hifiberry-debug-sysinfo-row')
-			block.appendChild(row)
-			create_line(row, info[i][0], info[i][1]);
+			if (info[i][0] == "Features" || 
+				info[i][0] == "Sound card") {
+				under = true;
+			} else {
+				under = false;
+			}
+			block.innerHTML += beo.createMenuItem({
+				label: info[i][0],
+				description: (under) ? info[i][1] : null,
+				value: (!under) ? info[i][1] : null,
+				static: true
+			}) + "\n";
+			
 		}
 	
 }
